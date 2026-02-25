eb140cb6b6723a8cb19768409e2cedaeaa5b9349
Ansh8905 <anshpandey1906@outook.com>
2026-02-26 03:19:48 +0530

    chore: configure deployment for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/PortfolioAnsh",
+  assetPrefix: "/PortfolioAnsh/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
